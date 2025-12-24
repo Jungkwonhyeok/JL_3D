f84a508c2bf985bfded1c7bfb7623d6aa0861f4b
Jungkwonhyeok <786poqqep@gmail.com>
2025-12-25 01:09:12 +0900

    재장전 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,7 @@ public class Player : MonoBehaviour
     public float jumpPower;
     public GameObject[] weapons; // 현재 캐릭터가 보유한 무기 오브젝트들
     public bool[] hasWeapons; // 무기(=캐릭터) 보유 여부
+    public Camera followCamera;
 
     public int ammo; //화살 개수
     public int coin; //코인 개수
@@ -139,6 +140,18 @@ public class Player : MonoBehaviour
             Quaternion targetRotation = Quaternion.LookRotation(move);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
+        if (fire1 && !isJump && !isReload && !isDodge && (equipWeapon != null && equipWeapon.curAmmo !=0))
+        {
+            Ray ray = followCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, 100))
+            {
+                Vector3 nextVec = rayHit.point - transform.position;
+                nextVec.y = 0f;
+                transform.LookAt(transform.position + nextVec);
+            }
+        }
+       
     }
 
     public void Jump() //점프
@@ -161,8 +174,10 @@ public class Player : MonoBehaviour
         fireDelay += Time.deltaTime; //공격 키를 느르고 얼마나 지났는지 값을 입력
         isFireReady = equipWeapon.rate < fireDelay; //공격 딜레이 < 공격 후 지난 시간 으로 공격 가능 여부 확인
 
-        if (fire1 && isFireReady && !isDodge && !isSwap) //공격이 가능 하고, 회피, 무기 교체 상태가 아닐 시 공격
+        if (fire1 && isFireReady && !isDodge && !isSwap && !isReload) //공격이 가능 하고, 회피, 무기 교체 상태가 아닐 시 공격
         {
+            if (equipWeapon.type == Weapon.Type.Range && equipWeapon.curAmmo <= 0)
+                return;
             equipWeapon.Use();
             anim.SetTrigger(equipWeapon.type == Weapon.Type.Melee ? "doSwing" : "doShot");
             fireDelay = 0;
@@ -176,7 +191,7 @@ public class Player : MonoBehaviour
 
         if (equipWeapon.type == Weapon.Type.Melee)
             return;
-        if (reload && !isJump && !isSwap && isFireReady)
+        if (reload && !isJump && !isSwap && isFireReady && ammo != 0 && equipWeapon.curAmmo != equipWeapon.maxAmmo && !isReload)
         {
             isReload = true;
             anim.SetTrigger("doReload");
@@ -413,10 +428,17 @@ public class Player : MonoBehaviour
         else if (hasWeapons[1] == true)
         {
             HeroChange(1); //도적
+            ammo += 50;
+            if (ammo >= maxAmmo)
+                ammo = maxAmmo;
+
         }
         else if (hasWeapons[2] == true)
         {
             HeroChange(2); //궁수
+            ammo += 50;
+            if (ammo >= maxAmmo)
+                ammo = maxAmmo;
         }
         else if (hasWeapons[3] == true)
         {
